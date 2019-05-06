cd79d87dccb1f5e73d41d13893d37cba09d22d76
siameseguy <psyhakhom@gmail.com>
2019-05-06 16:02:14 -0400

    included integration tests

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,5 @@
 import {combineReducers} from 'redux';
 import success from './successReducer';
+import guessedWordsReducer from './guessedWordsReducer';
 
-export default combineReducers({
-  success,
-});
+export default combineReducers({success, guessedWordsReducer});
