34481586085a47f15929a39a83ecfff6506fed01
Phanousit Syhakhom <psyhakhom@gmail.com>
2019-05-06 07:39:27 -0400

    added styles

--- src/Components/GuessedWords/GuessedWords.js
+++ src/Components/GuessedWords/GuessedWords.js
@@ -9,6 +9,29 @@ const GuessedWords = (props) => {
         Try to guess the secret word!
       </span>
     );
+  } else {
+    const guessedWords = props.guessedWords.map((word, index) => (
+      <tr data-test="guessed-word">
+        <td>{word.guessedWord}</td>
+        <td>{word.letterMatchCount}</td>
+      </tr>
+    ))
+    contents = (
+      <div data-test="guessed-words">
+        <h3>Guessed Words</h3>
+        <table className="table table-sm">
+          <thead className="thead-light">
+            <tr>
+              <th>Guess</th>
+              <th>Matching Letters</th>
+            </tr>
+          </thead>
+          <tbody>
+            {guessedWords}
+          </tbody>
+        </table>
+      </div>
+    )
   }
   return (
     <div data-test="component-guessed-words">
